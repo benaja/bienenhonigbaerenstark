3183832cf2c57e7ea979fae1af1caf4b4462830c
Hunzinger Benaja <Benaja.Hunzinger@swisscom.com>
2018-01-27 13:18:16 +0100

    website add

---
+++ view/default_index.php
@@ -0,0 +1,39 @@
+<!--
+<div id="header">
+  <div id="navigationbar">
+    <ul id="navigation_list">
+      <li>Startseite</li>
+      <li>Über Uns</li>
+      <li>Produkte</li>
+      <li>Kontakt</li>
+    </ul>
+  </div>
+</div>
+<div id="wallpaper">
+  <h1 id="titel">Schweizer Bienenhonig</h1>
+</div>
+
+<div id="content">
+  <h2>Honig in alles Sorten</h2>
+  <p>Wir bieten eine menge an unterschiedlichen Honigsorten an.</p>
+  <div id="honigsorten">
+    <div class="honigsorte">
+      <h3>Blütenhonig</h3>
+      <p>Zärtlich cremiger Honig, von blühenden Rapsfelder</p>
+      <img src="/images/blütenhonig.png">
+    </div>
+    <div class="honigsorte">
+      <h3>Waldhonig</h3>
+      <p>Feinster Honig aus den Wäldern.</p>
+      <img src="/images/waldhonig.png">
+    </div>
+    <div class="honigsorte">
+        <h3>Bergblütenhonig</h3>
+        <p>Honig aus unseren Schweizer Alpen</p>
+        <img src="/images/bergblütenhonig.jpg">
+      </div>
+  </div>
+  <h2 id="unsere_imkerei">Unsere Imkerei</h2>
+</div>
+-->
+<iframe id="iframe" src=""></iframe>
