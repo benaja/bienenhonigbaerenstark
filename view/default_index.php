45b65d6f9e9b80c14072652bd3464c21d0545b30
benaja <benhu00@outlook.com>
2018-01-27 14:39:49 +0100

    frontpage changes

--- view/default_index.php
+++ view/default_index.php
@@ -1,5 +1,9 @@
-
 <div id="header">
+  <img id="logo" src="/images/logo.png">
+  <div id="login">
+    <img src="/images/locked.svg">
+    <p>Login</p>
+  </div>
   <div id="navigationbar">
     <ul id="navigation_list">
       <li>Startseite</li>
@@ -14,25 +18,32 @@
 </div>
 
 <div id="content">
-  <h2>Honig in allen Sorten</h2>
-  <p>Wir bieten eine menge an unterschiedlichen Honigsorten an.</p>
+  <div class="content">
+    <h2>Honig in allen Sorten</h2>
+    <p>Wir bieten eine menge an unterschiedlichen Honigsorten an.</p>
+  </div>
   <div id="honigsorten">
     <div class="honigsorte">
-      <h3>Blütenhonig</h3>
-      <p>Zärtlich cremiger Honig, von blühenden Rapsfelder</p>
-      <img src="/images/blütenhonig.png">
+      <div class="honigsorte_inhalt">
+        <h3>Blütenhonig</h3>
+        <p>Zärtlich cremiger Honig, von blühenden Rapsfelder</p>
+        <img src="/images/blütenhonig.png">
+      </div>
     </div>
     <div class="honigsorte">
-      <h3>Waldhonig</h3>
-      <p>Feinster Honig aus den Wäldern.</p>
-      <img src="/images/waldhonig.png">
+      <div class="honigsorte_inhalt">
+        <h3>Waldhonig</h3>
+        <p>Feinster Honig aus den Wäldern.</p>
+        <img src="/images/waldhonig.png">
+      </div>
     </div>
     <div class="honigsorte">
+      <div class="honigsorte_inhalt">
         <h3>Bergblütenhonig</h3>
         <p>Honig aus unseren Schweizer Alpen</p>
         <img src="/images/bergblütenhonig.jpg">
       </div>
+    </div>
   </div>
   <h2 id="unsere_imkerei">Unsere Imkerei</h2>
 </div>
-
