52e664e0b325a0aa0120b5f202dea6108431e364
Hunzinger Benaja <Benaja.Hunzinger@swisscom.com>
2018-02-13 08:08:14 +0100

    bilder add

--- view/produkte.php
+++ view/produkte.php
@@ -20,50 +20,114 @@
 <div class="content_div">
     <div class="content">
         <h2 class="produkt_sorte_titel">Honige</h2>
-        <div class="produkt">
-            <div class="hover_produkt"></div>
-            <img src="/images/blütenhonig.jpg">
-            <div class="produkt_details">
-                <h3>Blütenhonig</h3>
-                <p class="preis">CHF 15.- / 500g</p>
+        <div id="honig">
+            <div class="produkt">
+                <div class="hover_produkt"></div>
+                <img src="/images/blütenhonig.jpg">
+                <div class="produkt_details">
+                    <h3>Blütenhonig</h3>
+                    <p class="preis">CHF 15.- / 500g</p>
+                </div>
+                <div class="warenkorb_button"></div>
             </div>
-            <div class="warenkorb_button"></div>
+            <div class="produkt">
+                <div class="hover_produkt"></div>
+                <img src="/images/honig01.jpg">
+                <div class="produkt_details">
+                    <h3>Bergblütenhonig </h3>
+                    <p class="preis">CHF 17.- / 500g</p>
+                </div>
+                <div class="warenkorb_button"></div>
+            </div>
+            <div class="produkt">
+                <div class="hover_produkt"></div>
+                <img src="/images/honig03.jpg">
+                <div class="produkt_details">
+                    <h3>Waldhonig</h3>
+                    <p class="preis">CHF 18.- / 500g</p>
+                </div>
+                <div class="warenkorb_button"></div>
+            </div>
+            <div class="produkt">
+                <div class="hover_produkt"></div>
+                <img src="/images/honig04.jpg">
+                <div class="produkt_details">
+                    <h3>Akatienhonig</h3>
+                    <p class="preis">CHF 15.- / 500g</p>
+                </div>
+                <div class="warenkorb_button"></div>
+            </div>
+            <div class="produkt">
+                <div class="hover_produkt"></div>
+                <img src="/images/honig05.jpg">
+                <div class="produkt_details">
+                    <h3>Sommerhonig</h3>
+                    <p class="preis">CHF 15.- / 500g</p>
+                </div>
+                <div class="warenkorb_button"></div>
+            </div>
         </div>
-        <div class="produkt">
-            <div class="hover_produkt"></div>
-            <img src="/images/honig01.jpg">
-            <div class="produkt_details">
-                <h3>Blütenhonig</h3>
-                <p class="preis">CHF 15.- / 500g</p>
+
+        <h2 class="produkt_sorte_titel">Kerzen</h2>
+        <div id="kerzen">
+            <div class="produkt">
+                <div class="hover_produkt"></div>
+                <img src="/images/kerzen01.jpg">
+                <div class="produkt_details">
+                    <h3>Standartkerze</h3>
+                    <p class="preis">CHF 5.- / Stück</p>
+                </div>
+                <div class="warenkorb_button"></div>
             </div>
-            <div class="warenkorb_button"></div>
+            <div class="produkt">
+                <div class="hover_produkt"></div>
+                <img src="/images/kerzen02.jpg">
+                <div class="produkt_details">
+                    <h3>Dekokerze</h3>
+                    <p class="preis">CHF 10.- / Stück</p>
+                </div>
+                <div class="warenkorb_button"></div>
+            </div>
+            <div class="produkt">
+                <div class="hover_produkt"></div>
+                <img src="/images/kerzen04.jpg">
+                <div class="produkt_details">
+                    <h3>Weihnachtskreze</h3>
+                    <p class="preis">CHF 15.- / Stück</p>
+                </div>
+                <div class="warenkorb_button"></div>
+            </div>
         </div>
-        <div class="produkt">
-            <div class="hover_produkt"></div>
-            <img src="/images/honig03.jpg">
-            <div class="produkt_details">
-                <h3>Blütenhonig</h3>
-                <p class="preis">CHF 15.- / 500g</p>
+
+        <h2 class="produkt_sorte_titel">Salben</h2>
+        <div id="salben">
+            <div class="produkt">
+                <div class="hover_produkt"></div>
+                <img src="/images/salbe01.jpg">
+                <div class="produkt_details">
+                    <h3>Mielason</h3>
+                    <p class="preis">CHF 20.- / Stück</p>
+                </div>
+                <div class="warenkorb_button"></div>
             </div>
-            <div class="warenkorb_button"></div>
-        </div>
-        <div class="produkt">
-            <div class="hover_produkt"></div>
-            <img src="/images/honig04.jpg">
-            <div class="produkt_details">
-                <h3>Blütenhonig</h3>
-                <p class="preis">CHF 15.- / 500g</p>
+            <div class="produkt">
+                <div class="hover_produkt"></div>
+                <img src="/images/salbe02.jpg">
+                <div class="produkt_details">
+                    <h3>DerMel</h3>
+                    <p class="preis">CHF 25.- / Stück</p>
+                </div>
+                <div class="warenkorb_button"></div>
             </div>
-            <div class="warenkorb_button"></div>
-        </div>
-        <div class="produkt">
-            <div class="hover_produkt"></div>
-            <img src="/images/honig05.jpg">
-            <div class="produkt_details">
-                <h3>Blütenhonig</h3>
-                <p class="preis">CHF 15.- / 500g</p>
+            <div class="produkt">
+                <div class="hover_produkt"></div>
+                <img src="/images/salbe03.jpg">
+                <div class="produkt_details">
+                    <h3>Mielason 2</h3>
+                    <p class="preis">CHF 19.- / Stück</p>
+                </div>
+                <div class="warenkorb_button"></div>
             </div>
-            <div class="warenkorb_button"></div>
         </div>
     </div>
 </div>
